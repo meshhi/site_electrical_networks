27869e790b423aeb4e255509e3d07de5b0660ca3
Michael <yumythug@yandex.ru>
2023-09-19 22:41:46 +0300

    main_window

--- src/components/StartWindow.jsx
+++ src/components/StartWindow.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react';
 
 export default ({changeOffset}) => {
 
-    const windowScroll = (e) => {
-      changeOffset('-100vh')
+    const windowScroll = (e, val) => {
+      changeOffset(val)
     }
 
     return(
-    <div className='main_window_container' onWheel={windowScroll}>
+    <div className='main_window_container' onWheel={(e) => windowScroll(e, '-100vh')}>
       <header className="main_window_header">
         <ul className="main_window_header__menu">
-          <li className="main_window_header__menu_item">О нас</li>
+          <li className="main_window_header__menu_item" onClick={(e) => windowScroll(e, '-100vh')}>О нас</li>
           <li className="main_window_header__menu_item">Услуги</li>
           <li className="main_window_header__menu_item">Контакты</li>
           <li className="main_window_header__menu_item">Другое</li>
         </ul>
       </header>
       <main className='main_window_content'>
+        <div className='main_window_content__inner_container'>
+
+        </div>
       </main>
       <footer className='main_window_footer'>
 
